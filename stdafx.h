8508b06c4c16226e9bf473ee28b5e1b7d75a2f35
ilixiaofu <1220324958@qq.com>
2019-11-17 19:25:49 +0800

    创建工程

---
+++ stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <windows.h>
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+#include "util\StringUtil.h"
+#include "..\UtilsLib\UtilsLib\utils.h"
+
+
+#define STRING_LINE				"\r\n------------------------------------------------------------"
+#define BACK_LAST_HOME          "\r\n[返回上级目录](./../../../../categories/default/README.md)&emsp;|&emsp;"
+#define BACK_HOME               "[返回首页](./../../../../README.md)"
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
